8ee10f6c97d828cee0ac3d406660371909cb137e
Vinicius Souza <visouza@microsoft.com>
2020-06-22 15:31:57 -0700

    ADD: ProviderConfiguration unit test

---
+++ src/AuthJanitor.Tests/ProviderConfigurationTests.cs
@@ -0,0 +1,37 @@
+﻿using AuthJanitor.Providers;
+using AuthJanitor.Providers.AppServices;
+using System;
+using Xunit;
+
+namespace AuthJanitor.Tests
+{
+    public class ProviderConfigurationTests
+    {
+        private readonly IProviderConfiguration providerConfiguration;
+
+        public ProviderConfigurationTests() {
+            providerConfiguration = new ProviderConfiguration();
+        }
+
+        [Fact]        
+        public void ReceivingNullMustReturnFalse()
+        {
+            Assert.False(providerConfiguration.IsConfigurationValid(null, null));
+            Assert.False(providerConfiguration.IsConfigurationValid(null, "{ \"Id\":\"Test\" }"));
+        }
+
+        [Fact]
+        public void ReturnTrueWhenReceivingValidConfiguraiton()
+        {
+            var providerMetadata = new LoadedProviderMetadata()
+            {
+                ProviderConfigurationType = typeof(AppSettingConfiguration),
+            };
+
+            providerConfiguration.IsConfigurationValid(providerMetadata, "{ \"SettingName\":\"Test\", \"CommitAsConnectionString\":\"false\" }");
+        }
+
+
+
+    }
+}
